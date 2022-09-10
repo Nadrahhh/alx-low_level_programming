e13c661e00fd7e87e86b8f52293b6ce4999a3850
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-09 20:25:12 -0700

    C is hard

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * The stribg last degit of n
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+	int lastn;
+
