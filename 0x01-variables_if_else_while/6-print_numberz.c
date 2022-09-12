bd8c00a1b17aa84aa59af6e92c03f78f5014eaa0
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-11 18:07:29 -0700

    good day

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
