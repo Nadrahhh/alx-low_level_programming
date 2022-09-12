36e8474e3d8fbad52fc507a4905e30e7f4ae8eaf
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-11 19:01:37 -0700

    good day

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int revlower;
+
+	revlower = 'z';
+	while (revlower >= 'a')
+	{
+		putchar (revlower);
+		revlower++;
+	}
+	putchar('\n');
+	return (0);
+}
