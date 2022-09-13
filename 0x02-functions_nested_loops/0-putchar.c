9319ff395947e35f261d2ac8717d05aeda8b9f17
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-13 14:38:00 -0700

    good evening

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+
+	char put = 'p'
+
+{
+	putchar("_putchar\n");
+	return (0);
+}
