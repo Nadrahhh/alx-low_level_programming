d0269d249774550d42f113eeb91c4d587e92abf3
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-14 04:00:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,7 @@
  * @c: is the int that use for the argument of the function
  * Return: 0
  */
-int _islower(int c);
-
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
