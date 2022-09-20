a162bdf3f71afcc339ef30e4db3cb458786830ae
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-19 18:36:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: printing strings
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
