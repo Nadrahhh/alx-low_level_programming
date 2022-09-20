2d321b9aac554ce2ff11a312b056b3a3502116d5
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-19 18:03:50 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * int _strlen - write a function that returns the lenght of a string
+ * _strlen - write a function that returns the lenght of a string
+ * @s: returning lenght of a string
  *
  * Return: 0 .
  */
