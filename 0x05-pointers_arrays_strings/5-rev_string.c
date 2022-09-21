049983a37cfab888394b251cb60ffc34b9686e67
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-20 21:47:38 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,8 +9,23 @@
 
 void rev_string(char *s)
 {
-	int rev = strlen(s);
+	int i, c, k;
+	char *a, aux;
 
-	while (rev--)
-		putchar(rev);
+	a = s;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
 }
