399a323607b1d1757f9d0afea491503833e7b3f8
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-22 09:47:55 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compare two strings
+ * @s1: strcmp
+ * @s2: compare using strcmp
+ *
+ * Return: int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, d = 0, lim;
+
+	while (s1[a])
+	{
+		a++;
+	}
+	while (s2[b])
+	{
+		b++;
+	}
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			d = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+}
