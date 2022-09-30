8a3d25e1cfbdff806034bd12d91cdfb70319f3a1
Nadrahhh <nadrahnajeemdeen@gmail.com>
2022-09-29 21:34:46 -0700

    “good”

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int real_root(int n, int i);
+
+/**
+ * _sqrt_recursion - reaturns
+ * @n: at n 
+ * Return: sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
